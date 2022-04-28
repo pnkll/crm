458468946ed6287491f911b72103ca4b06c29f52
user <kavasakya@gmail.com>
2022-04-28 09:17:20 +0300

    temp

--- src/components/Tasks/TaskForm/TaskForm.jsx
+++ src/components/Tasks/TaskForm/TaskForm.jsx
@@ -1,26 +1,18 @@
 import { Form, Input, Button, Select, Space, Typography, Upload, } from 'antd';
 import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
+import 'react-day-picker/dist/style.css';
+import { DayPicker } from 'react-day-picker';
+import React, { useState } from 'react';
 
 const TaskForm = () => {
 
     const onFinish = (data) => {
         const value = {
-            ...data,
-            'date-picker': data['date-picker'].format('YYYY-MM-DD'),
+            ...data, day: date
         }
         console.log(value)
     }
-
-    const config = {
-        rules: [
-            {
-                type: 'object',
-                required: true,
-                message: 'Please select time!',
-            },
-        ],
-    };
 
     const normFile = (e) => {
         console.log('Upload event:', e);
@@ -31,6 +23,14 @@ const TaskForm = () => {
 
         return e && e.fileList;
     };
+
+    const [selected, setSelected] = useState()
+
+    let footer = <p>Please pick a day</p>
+
+    if (selected) {
+        footer = <p>You picked {format(selected, 'PP')}.</p>;
+      }
 
     return <>
         <Form onFinish={onFinish}>
@@ -47,11 +47,6 @@ const TaskForm = () => {
             >
                 <Input.TextArea showCount maxLength={100} />
             </Form.Item>
-            <Form.Item label="Select">
-                <Select>
-                    <Select.Option value="demo">Demo</Select.Option>
-                </Select>
-            </Form.Item>
             <Form.Item
                 name="upload"
                 valuePropName="fileList"
@@ -61,63 +56,15 @@ const TaskForm = () => {
                     <Button icon={<UploadOutlined />}>Прикрепить файл</Button>
                 </Upload>
             </Form.Item>
+            <Form.Item name="data"><DayPicker mode="single" mode="single"
+                selected={selected}
+                onSelect={setSelected}
+                footer={footer} /></Form.Item>
             <Form.Item colon={false}>
                 <Button type="primary" htmlType="submit">
                     Сохранить
                 </Button>
             </Form.Item>
-            {/* <Form.Item label="Username">
-                <Space>
-                    <Form.Item
-                        name="username"
-                        noStyle
-                        rules={[{ required: true, message: 'Username is required' }]}
-                    >
-                        <Input style={{ width: 160 }} placeholder="Please input" />
-                    </Form.Item>
-                    <Tooltip title="Useful information">
-                        <Typography.Link href="#API">Need Help?</Typography.Link>
-                    </Tooltip>
-                </Space>
-            </Form.Item>
-            <Form.Item label="Address">
-                <Input.Group compact>
-                    <Form.Item
-                        name={['address', 'province']}
-                        noStyle
-                    >
-                        <Select placeholder="Select province">
-                            <Option value="Zhejiang">Zhejiang</Option>
-                            <Option value="Jiangsu">Jiangsu</Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item
-                        name={['address', 'street']}
-                        noStyle
-                        rules={[{ required: true, message: 'Street is required' }]}
-                    >
-                        <Input style={{ width: '50%' }} placeholder="Input street" />
-                    </Form.Item>
-                </Input.Group>
-            </Form.Item>
-            <Form.Item label="BirthDate" style={{ marginBottom: 0 }}>
-                <Form.Item
-                    name="year"
-                    rules={[{ required: true }]}
-                    style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
-                >
-                    <Input placeholder="Input birth year" />
-                </Form.Item>
-                <Form.Item
-                    name="month"
-                    rules={[{ required: true }]}
-                    style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '0 8px' }}
-                >
-                    <Input placeholder="Input birth month" />
-                </Form.Item>
-            </Form.Item>
-             */}
-
         </Form>
     </>
 }
